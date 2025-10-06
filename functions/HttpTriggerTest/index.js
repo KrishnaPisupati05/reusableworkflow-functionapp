d85bbac28375f5fd116eb6a01c45438cec3cda29
Pisupati <Krishna.Pisupati@sas.dk>
2025-10-06 15:15:40 +0530

    New code added

--- functions/HttpTriggerTest/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-    context.log('HTTP trigger function processed a request.');
-    const name = req.query.name || (req.body && req.body.name) || 'World';
-    context.res = {
-        body: `Hello, ${name}! Function is running.`
-    };
-};
